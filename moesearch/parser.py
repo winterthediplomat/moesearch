36eb226db185ffb7d373f52a812f9320bbec4ca0
alfateam123 <alfateam123@gmail.com>
2014-08-27 18:24:11 +0200

    new Board object, bugfix on media=null, thread!=index
    * Add Board object
    * bugfix: In a post, if "media" is "null", it does no longer crash.
    * bugfix: Response from /thread and /index is different.
    As we reuse the Thread object, we need to handle the
    different behaviour.

--- moesearch/parser.py
+++ moesearch/parser.py
@@ -2,23 +2,46 @@
 
 from pprint import pprint
 from .exceptions import ExceptionFactory
+from collections import namedtuple
+
+Board = namedtuple("Board", "short_name name")
 
 class Post(object):
   def __init__(self, post_dict):
     for post_key in post_dict:
       if post_key == "media":
         self.__dict__[post_key] = Media(post_dict[post_key])
+      elif post_key == "board":
+        self.__dict__[post_key] = Board(post_dict[post_key]["shortname"],
+                                        post_dict[post_key]["name"])
       else:
         self.__dict__[post_key] = post_dict[post_key]
 
+  def __repr__(self):
+    return repr(self.__dict__)
+
 class Thread(object):
   def __init__(self, thread_dict):
+    #pprint(thread_dict)
+    try:
+      self.thread_number = -1
+      self.build_thread(thread_dict)
+    except KeyError:
+      #when requesting with /thread?, we have {"threadnum": threadobj}
+      #when working on /index? results, it seems not.
+      self.thread_number = list(thread_dict.keys())[0]
+      self.build_thread(thread_dict[self.thread_number])
+
+  def build_thread(self, thread_dict):
     self.op = Post(thread_dict["op"])
     try:
       thread_dict["posts"] = map(Post,thread_dict["posts"]) 
     except KeyError:
       thread_dict["posts"] = list()
     self.posts = thread_dict["posts"]
+
+  def __repr__(self):
+    return repr(self.__dict__)
 
 class IndexResult(Thread):
   def __init__(self, thread_dict):
@@ -30,5 +53,9 @@ class IndexResult(Thread):
 
 class Media(object):
   def __init__(self, post_dict):
-    for post_key in post_dict:
+    #if the post has no image/video, post_dict is None.
+    for post_key in (post_dict or dict()):
       self.__dict__[post_key] = post_dict[post_key]   
+
+  def __repr__(self):
+    return repr(self.__dict__)
