10d191432b4bb65714a2a692138495acd8862f05
alfateam123 <alfateam123@gmail.com>
2014-08-27 18:22:21 +0200

    Add PostNotFound exception, GenericError now tells you the error
    * Add PostNotFound exception
    * In the text of GenericError, you can find the server response

--- moesearch/exceptions.py
+++ moesearch/exceptions.py
@@ -8,18 +8,23 @@ def is_error(error_dict):
 class ExceptionFactory(object):
   @classmethod
   def generateException(self, error_dict):
-    #print("[ExceptionFactory.generateException] error_dict", error_dict)
+    print("[ExceptionFactory.generateException] error_dict", error_dict)
     #print(type(error_dict))
     if error_dict["error"] == "No board selected.":
       return BoardNotFound("You have not selected a board, or the board you're looking for does not exist")
+    elif error_dict["error"] == "Post not found.":
+      return PostNotFound("Post not found")
     else:
-      return GenericError("Ehm... Please, go search some Jibril posts on /a/ while I figure it out.")
+      return GenericError("Ehm... Please, go search some Jibril posts on /a/ while I figure it out: "+repr(error_dict))
 
 class ArchiveException(Exception):
   pass
 
 class BoardNotFound(ArchiveException):
-  """ the selected board does not exist """
+  """ the selected board does not exist or you have not selected a board """
+
+class PostNotFound(ArchiveException):
+  """ the selected post does not exist """
 
 class GenericError(ArchiveException):
   """ ask Woxxy about this. """
