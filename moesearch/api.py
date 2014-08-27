b075deb6cbe03f5132a9c918570bc1c60361b216
alfateam123 <alfateam123@gmail.com>
2014-08-27 18:27:44 +0200

    Now using the full power of requests (aka: I'm dumb)
    Now using the "parameters" parameter when issuing an HTTP request.

--- moesearch/api.py
+++ moesearch/api.py
@@ -6,27 +6,39 @@ from .exceptions import is_error, ExceptionFactory
 from pprint import pprint
 
 def index(board, page):
-  res = requests.get("http://api.archive.moe/index?board=%s&page=%d"%(board, int(page)), stream=False).json()
+  res = requests.get("https://api.archive.moe/index", stream=False, 
+          params = {"board": board, "page": int(page)}).json()
   if is_error(res):
     raise ExceptionFactory.generateException(res)
   for thread_num in res:
     res[thread_num] = IndexResult(res[thread_num])
   return res 
 
-def search(board, *args):
-  defaults = {"email":"", "username":"",
-  "tripcode":"", "capcode":"", "subject":"",
-  "text":"", "filename":"", "filehash":"",
-  "deleted":-1, "ghost":-1, "filter":-1,
-  "date_start":"", "date_end":"", "order":""}
-  pass
-  
+def search(board, **kwargs):
+  url = "https://api.archive.moe/search"
+  #print("[api.search] url", url)
+  kwargs["board"]=board
+  res = requests.get(url, stream=False, params=kwargs).json()
+  if is_error(res):
+    raise ExceptionFactory.generateException(res)
+  res = res[0]
+  return map(Post, res["posts"])
 
 def thread(board, thread_num, latest_doc_id=-1, last_limit=-1):
-  pass
+  url = "https://api.archive.moe/thread"
+  payload = {"board": board, "num": thread_num}
+  if(latest_doc_id != -1):
+    payload["latest_doc_id"] = (int(latest_doc_id))
+  if(last_limit != -1):
+    payload["last_limit"] = (int(last_limit))
+  res = requests.get(url, stream=False, params=payload).json()
+  if is_error(res):
+    raise ExceptionFactory.generateException(res)
+  return Thread(res)
 
 def post(board, post_num):
-  res = requests.get("http://api.archive.moe/post?board=%s&num=%d"%(board, int(post_num)), stream=False)
+  res = requests.get("https://api.archive.moe/post", stream=False,
+                  params={"board":board, "num":post_num}).json()
   if is_error(res):
     raise ExceptionFactory.generateException(res)
   return Post(res)
