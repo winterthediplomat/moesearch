dbfa9092a9a60aca17be47d19c7c6254304a1a23
alfateam123 <alfateam123@gmail.com>
2014-08-27 16:10:04 +0200

    Force "num" type, implement api.post
    * Add api.post handling
    * Force the "num" type in both Post and Index requests

--- moesearch/api.py
+++ moesearch/api.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
 import requests
-#from parser import Post, IndexResult
-from .parser import IndexResult
+from .parser import *
 from .exceptions import is_error, ExceptionFactory
-#from json import loads
 from pprint import pprint
 
 def index(board, page):
-  res = requests.get("http://api.archive.moe/index?board=%s&page=%d"%(board, page), stream=False).json()
+  res = requests.get("http://api.archive.moe/index?board=%s&page=%d"%(board, int(page)), stream=False).json()
   if is_error(res):
-    #print(res)
-    raise ExceptionFactory.generateException(res) #ValueError("got an error!")
+    raise ExceptionFactory.generateException(res)
   for thread_num in res:
     res[thread_num] = IndexResult(res[thread_num])
   return res 
@@ -29,4 +26,7 @@ def thread(board, thread_num, latest_doc_id=-1, last_limit=-1):
   pass
 
 def post(board, post_num):
-  pass
+  res = requests.get("http://api.archive.moe/post?board=%s&num=%d"%(board, int(post_num)), stream=False)
+  if is_error(res):
+    raise ExceptionFactory.generateException(res)
+  return Post(res)
