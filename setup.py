14c95b805dde754af9978883987bdbe8c3d4b561
alfateam123 <alfateam123@gmail.com>
2014-08-28 13:33:01 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='moesearch',
-    version='0.1.1',
+    version='0.1.2',
     author='alfateam123',
     author_email='alfateam123@gmail.com',
     packages=['moesearch'],
@@ -15,4 +15,11 @@ setup(
     install_requires=[
         "requests >= 2.3.0"
     ],
+    classifiers=[
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
