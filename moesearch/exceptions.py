76edb7e59743577e106f49b477066c07ef261fb5
alfateam123 <alfateam123@gmail.com>
2014-11-20 20:27:19 +0100

    remove print of returned json error

--- moesearch/exceptions.py
+++ moesearch/exceptions.py
@@ -8,7 +8,7 @@ def is_error(error_dict):
 class ExceptionFactory(object):
   @classmethod
   def generateException(self, error_dict):
-    print("[ExceptionFactory.generateException] error_dict", error_dict)
+    #print("[ExceptionFactory.generateException] error_dict", error_dict)
     #print(type(error_dict))
     if error_dict["error"] == "No board selected.":
       return BoardNotFound("You have not selected a board, or the board you're looking for does not exist")
