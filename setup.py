6475ffc3fd09a2a741199166de857c5a4c0dacd5
alfateam123 <alfateam123@gmail.com>
2014-11-24 16:31:34 +0100

    Bump version number after "rough fix to ResourceWarning"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='moesearch',
-    version='0.1.3',
+    version='0.1.4',
     author='alfateam123',
     author_email='alfateam123@gmail.com',
     packages=['moesearch'],
