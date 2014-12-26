ecfcba2ad7a80e4586cb2d5a83c26ec41ff22b35
alfateam123 <alfateam123@gmail.com>
2014-12-26 12:30:26 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='moesearch',
-    version='0.1.7',
+    version='0.1.8',
     author='alfateam123',
     author_email='alfateam123@gmail.com',
     packages=['moesearch'],
